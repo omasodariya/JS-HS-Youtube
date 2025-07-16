d961c8b9264bd5cdb89cea5c15ad31778de197b5
omasodariya <omasodariya09@gmail.com>
2025-07-16 03:38:15 +0000

    02 Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("Hello") // we are using nodejs, not browser
+
+console.log(3+3) // Code readability should be high
+
+console.log("Om")
+
+let name="Om"
+let age=19
+let isLoggedIn=false
+
+// number => 2 to power 53
+// bigInt
+// String => ""
+// boolean => true / false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// Object
+
+console.log(typeof "om");
+console.log(typeof age);
+console.log(typeof null); //type object 
+console.log(typeof undefined);
+
+
