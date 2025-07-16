9c3498bbfb061d5ea956fac26c32f90dbef7fb77
omasodariya <omasodariya09@gmail.com>
2025-07-16 04:04:34 +0000

    03 OPERATION CONFUSION

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,7 +1,7 @@
 // let score="33abc"
 // let score=null
 // let score=undefined
-let score=false
+// let score=false
 
 // "33" =>33
 // "33abc" =>NaN
@@ -14,15 +14,51 @@ let score=false
 // console.log(typeof valueInNumber);
 // console.log(valueInNumber);
 
-let isLoggedIn="om"
-let BooleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(BooleanIsLoggedIn);
+// let isLoggedIn="om"
+// let BooleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(BooleanIsLoggedIn);
 
 //1 => true; 0=> false
 // ""=>false
 // "Om" => true
 
-let num=33
-let stringNum=String(num)
-console.log(stringNum);
-console.log(typeof stringNum);
+// let num=33
+// let stringNum=String(num)
+// console.log(stringNum);
+// console.log(typeof stringNum);
+
+// **************************** Operations **************************
+
+// let value=3;
+// let negVal=-value
+// console.log(negVal);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(20%3);
+
+// let str1="Hello"
+// let str2=" Om"
+// let str3= str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + "2");
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1,num2,num3
+// num1=num2=num3=2+2
+
+
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter);
+
